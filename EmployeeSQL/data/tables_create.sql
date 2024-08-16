1c44d5d3b09d1ce19ad7f6344fc627f057db9d6a
Rinal Shastri <rinal.shastri@outlook.com>
2024-08-16 13:11:47 -0700

    ERD

--- EmployeeSQL/data/tables_create.sql
+++ EmployeeSQL/data/tables_create.sql
@@ -1,52 +1,48 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
-
 CREATE TABLE "titles" (
-    "title_id" string   NOT NULL,
-    "title" string   NOT NULL,
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(50)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
 CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "emp_title_id" string   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(10)   NOT NULL,
     "birth_date" date   NOT NULL,
-    "first_name" string   NOT NULL,
-    "last_name" string   NOT NULL,
-    "sex" char   NOT NULL,
-    "hire_date" date   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
 CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
 );
 
 CREATE TABLE "departments" (
-    "dept_no" string   NOT NULL,
-    "dept_name" string   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
 CREATE TABLE "dept_managers" (
-    "dept_no" string   NOT NULL,
-    "emp_no" int   NOT NULL
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT   NOT NULL
 );
 
 CREATE TABLE "dept_emp" (
-    "dept_no" string   NOT NULL,
-    "emp_no" string   NOT NULL
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT NOT NULL
 );
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
